a14d6d175cdb922b35fc5e5f9ee3363828f8c642
Evgenii Sopov <mrseakg@gmail.com>
2026-01-31 18:05:53 +0700

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,18 @@
 #include <wsjcpp_sql_builder.h>
 
 int main(int argc, const char* argv[]) {
-    // WsjcppSqlBuilderInsert sql("TABLE_NAME");
-    // sql.add("COL1", "val1"); // will be escaped
-    // sql.add("COL2", 1);
-    // // sql.add("COL3", 1.1);
-    // if (!sql.isValid()) {
-    //     std::cerr << "Something wrong with query: " << sql.getErrorMessage() << std::endl;
-    //     return -1;
-    // }
 
-    // std::string expectedIns = "INSERT INTO TABLE_NAME(COL1, COL2) VALUES ('val1', 1);";
-    // if (expectedIns != sql.getTextQuery()) {
-    //     std::cerr << "Expeceted sql: " << expectedIns << ", but got " << sql.getTextQuery() << std::endl;
-    //     return -1;
-    // }
-    // std::cout << sql.getTextQuery() << std::endl;
+    wsjcpp::SqlBuilder builder;
+        builder.selectFrom("users")
+        .colum("id")
+        .colum("name")
+        .colum("pass")
+        .where()
+            .equal("pass", "64d878e22cd5046d569bf826869034caf5feab24")
+    ;
+
+    std::string sql = builder.sql();
+    std::cout << sql << std::endl;
 
     return 0;
 }
